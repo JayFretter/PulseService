2422cecc3bd260104a34e9021c8f4f2159415f3d
Jay Fretter <jhfretter23@gmail.com>
2022-12-27 20:16:20 +0000

    Created BiscuitController

---
+++ BiscuitService/Controllers/BiscuitController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BiscuitService.Controllers
+{
+    [ApiController]
+    [Route("biscuits")]
+    public class BiscuitController: ControllerBase
+    {
+        private readonly ILogger<BiscuitController> _logger;
+
+        public BiscuitController(ILogger<BiscuitController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public bool Create()
+        {
+            _logger.LogInformation("Creating a new Biscuit");
+
+            return true;
+        }
+    }
+}
