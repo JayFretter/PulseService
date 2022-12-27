ddf1b2c64755d9795bb3a2184825559e86a5cc68
Jay Fretter <jhfretter23@gmail.com>
2022-12-27 21:48:43 +0000

    Moved BiscuitDbo to models folder

--- BiscuitService.DatabaseAdapter.Mongo/Models/BiscuitDbo.cs
+++ BiscuitService.DatabaseAdapter.Mongo/Models/BiscuitDbo.cs
@@ -2,7 +2,7 @@
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-namespace BiscuitService.DatabaseAdapter.Mongo
+namespace BiscuitService.DatabaseAdapter.Mongo.Models
 {
     public class BiscuitDbo
     {
