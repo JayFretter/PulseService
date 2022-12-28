1781239b0c2a2096b9bbcad0bc3fb76cca0a96db
Jay Fretter <jhfretter23@gmail.com>
2022-12-28 18:06:03 +0000

    Created login credentials object

---
+++ BiscuitService/Mappers/LogInUserQueryMapper.cs
@@ -0,0 +1,17 @@
+﻿using BiscuitService.Domain.Models;
+using BiscuitService.Models.Queries;
+
+namespace BiscuitService.Mappers
+{
+    public static class LogInUserQueryMapper
+    {
+        public static UserCredentials ToDomain(this LogInUserQuery query) 
+        {
+            return new UserCredentials
+            {
+                Username = query.Username,
+                Password = query.Password,
+            };
+        }
+    }
+}
