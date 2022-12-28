a3afdfeae030044d05c7add356b457891cc46664
Jay Fretter <jhfretter23@gmail.com>
2022-12-28 14:24:56 +0000

    Created domain models for a User and a Vote

---
+++ BiscuitService.Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BiscuitService.Domain.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime CreatedAtUtc { get; set; }
+        public IEnumerable<Vote> Votes { get; set; } = new List<Vote>();
+    }
+}
---
+++ BiscuitService.Domain/Models/Vote.cs
@@ -0,0 +1,8 @@
+﻿namespace BiscuitService.Domain.Models
+{
+    public class Vote
+    {
+        public string BiscuitId { get; set; } = string.Empty;
+        public string OptionName { get; set; } = string.Empty;
+    }
+}
