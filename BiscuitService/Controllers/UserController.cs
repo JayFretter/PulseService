11de7f566da6eb34f892772a7be3021f89b0f152
Jay Fretter <jhfretter23@gmail.com>
2022-12-28 14:24:45 +0000

    Created UserController with base CreateUser method

---
+++ BiscuitService/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using BiscuitService.Models.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiscuitService.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult CreateUser([FromBody] CreateUserQuery newUser)
+        {
+            _logger.LogInformation("Creating new user {name}", newUser.Username);
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to create user {name}", newUser.Username);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+    }
+}
