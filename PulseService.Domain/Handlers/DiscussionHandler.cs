1fb8dd9b5970ff2d72ab6ba4da9391c904231b8c
Jay Fretter <jhfretter23@gmail.com>
2023-04-17 22:46:05 +0100

    Created shell discussion handler

---
+++ PulseService.Domain/Handlers/DiscussionHandler.cs
@@ -0,0 +1,12 @@
+﻿using PulseService.Domain.Models;
+
+namespace PulseService.Domain.Handlers
+{
+    public class DiscussionHandler : IDiscussionHandler
+    {
+        public Task CreateDiscussionCommentAsync(DiscussionComment discussionComment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PulseService.Domain/Handlers/IDiscussionHandler.cs
@@ -0,0 +1,9 @@
+﻿using PulseService.Domain.Models;
+
+namespace PulseService.Domain.Handlers
+{
+    public interface IDiscussionHandler
+    {
+        Task CreateDiscussionCommentAsync(DiscussionComment discussionComment);
+    }
+}
