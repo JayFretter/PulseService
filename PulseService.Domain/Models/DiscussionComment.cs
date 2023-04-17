8beb197c7fdc58b95c05d16f0153f739ce96f419
Jay Fretter <jhfretter23@gmail.com>
2023-04-17 22:02:35 +0100

    Create DiscussionComment.cs

---
+++ PulseService.Domain/Models/DiscussionComment.cs
@@ -0,0 +1,14 @@
+﻿namespace PulseService.Domain.Models
+{
+    public class DiscussionComment
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string OpinionName { get; set; } = string.Empty;
+        public string OpinonBody { get; set; } = string.Empty;
+        public string PulseId { get; set; } = string.Empty;
+        public uint Upvotes { get; set; }
+        public uint Downvotes { get; set; }
+        public IEnumerable<DiscussionComment> Children { get; set; } = new List<DiscussionComment>();
+    }
+}
